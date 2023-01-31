28dc407045c040ca1c7f621126431f9eb985da4a
behzad dariush <behzaddariush@gmail.com>
2023-02-01 01:28:55 +0330

    add class

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,93 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections.Immutable;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
 using static System.Net.Mime.MediaTypeNames;
-//// i is input counter
-int i = 0;
+////studentnum is input counter
+///
 
+int returnto = 0;
+string UserChoose;
+string studentcode;
+
+
+while (true)
+{
+    Console. WriteLine(" Which one you whant to choose :\n1: class \n2: student \n3: Exit");
+    Console.Write("UR choose: ");
+    UserChoose = Console.ReadLine();
+    List<Classroom> Classrooms = new List<Classroom>();
+    if (UserChoose == "1" || UserChoose == "class")
+
+        Console.Clear();
+        Console.WriteLine("------------ you are in class part --------------");
+    {
+        while (returnto < 1)
+        {
+            Console.WriteLine(" Which one you whant to choose :\n1: <Make> a class \n2: <Add> student to class by student code" +
+           "\n3: show student to specific class\n4 show class <list> \n5: <return>");
+            Console.Write("UR choose: ");
+            UserChoose = Console.ReadLine();
+            if (UserChoose == "1" || UserChoose == "make")
+            {
+                Console.Write("Your classname you want to add is : ");
+                UserChoose = Console.ReadLine();
+                Classroom newclassroom = new Classroom();
+                newclassroom.classname = UserChoose.ToString();
+                Classrooms.Add(new Classroom() { classname = newclassroom.classname.ToUpper() });
+
+                Console.ReadLine();
+
+            }
+            else if (UserChoose == "2" || UserChoose == "add")
+            {
+                if (Classrooms.Count() == 0)
+                {
+                    Console.WriteLine("there is no class make yet !");
+                }
+                else
+                {
+                    Classrooms.ForEach(Classrooms => Console.WriteLine(Classrooms.classname));
+                    Console.Write("which class you want to add student ? ");
+                    UserChoose = Console.ReadLine();
+                    Console.Write("enter student code :  ");
+                    studentcode = Console.ReadLine();
+
+                }
+                Console.Write("Your classname you want to add is : ");
+                UserChoose = Console.ReadLine();
+                Classrooms.ForEach(Classrooms => Console.WriteLine(Classrooms.classname));
+                Console.ReadLine();
+
+            }
+            else if (UserChoose == "4" || UserChoose == "list")
+            {
+
+                Classrooms.ForEach(Classrooms => Console.WriteLine(Classrooms.classname));
+                Console.ReadLine();
+
+            }
+            else if (UserChoose == "5" || UserChoose == "return")
+            {
+                returnto = 1;
+                Console.Write(returnto);
+                Console.Clear();
+            }
+        }
+    }
+}
+
+
+
+/*
+int studentnum = 0;
 List<Student> students = new List<Student>();
 do
 {
     Console.WriteLine(" input a number:\n1: Adding \n2: Show all\n3: search \n4: Exit");
-    int index = 0;
+    
     string b;
     Console.Write("UR Number: ");
     b = Console.ReadLine();
@@ -20,13 +98,13 @@ do
         if (userEnter == 1)
         {
 
-            i++;
+            studentnum++;
             Student mystudent = new Student();
-            Console.Write("inter student code : ");
+            Console.Write("enter student code : ");
             mystudent.StudentCode = Console.ReadLine().ToString();
-            Console.Write("inter student first name : ");
+            Console.Write("enter student first name : ");
             mystudent.FirstName = Console.ReadLine();
-            Console.Write("inter student last name : ");
+            Console.Write("enter student last name : ");
             mystudent.LastName = Console.ReadLine();
             students.Add(new Student() { FirstName = mystudent.FirstName, LastName = mystudent.LastName, StudentCode = mystudent.StudentCode });
 
@@ -34,8 +112,9 @@ do
         }
         else if (userEnter == 2)///// show the list
         {
+            int index = 0;
             Console.Clear();
-            while (index < i)
+            while (index < studentnum)
             {
                 Console.WriteLine(students[index].FirstName + "  " + students[index].LastName + "  " + students[index].StudentCode);
                 index = index + 1;
@@ -50,10 +129,10 @@ do
         {
             Console.Clear();
             Console.Write("PLZ Enter a name that you want to search : ");
-            string letter;
-            letter = Console.ReadLine();
+            
+            string letter = Console.ReadLine();
             int counter = 0;            
-            for (int y = 0; y < i; y++)
+            for (int y = 0; y < studentnum; y++)
             {
                 
                 if (students[y].FirstName.StartsWith (letter))
@@ -65,7 +144,7 @@ do
             }
             if (counter == 0)
             {
-                Console.WriteLine("The name does match");
+                Console.WriteLine("The name does not match");
             }
             Console.ReadLine();
         }
@@ -107,4 +186,25 @@ class Student
     }
 }
 
+*/
+class Classroom
+{
+    public string classname { get; set; }
+   
 
+    public string classroom()
+    {
+        return (classname);
+
+    }
+    public string classstudent()
+    {
+        return (classname + stdentcode + firsname + lastname);
+    }
+
+    public string stdentcode { get; set; }
+    public string firsname {get; set; }
+    public string lastname { get; set; }
+   
+    
+}
